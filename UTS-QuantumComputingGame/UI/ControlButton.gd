be5c945002692158f91786d94bd82161fa001e0c
Aryeh <aryehzinn@hotmail.com>
2019-12-03 14:12:41 +1100

    Added a new UI button for the control and removed the third qbit due to complications in the maths.

---
+++ UTS-QuantumComputingGame/UI/ControlButton.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+onready var button = LogicGateButton.new()
+var loaded_asset = preload("res://LogicGates/ControlGate.tscn")
+
+func on_click(mouse_pos, parent):
+	z_index = 1
+	var gate = button.spawn_gate(mouse_pos, parent, loaded_asset)
+	return gate
