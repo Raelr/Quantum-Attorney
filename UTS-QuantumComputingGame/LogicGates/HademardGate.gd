e41a20b50fd9a875069ea4db86a36996d396dab6
Aryeh <aryehzinn@hotmail.com>
2019-12-03 12:05:30 +1100

    Added more aesthetics and started adding gates to buttons

--- UTS-QuantumComputingGame/LogicGates/HademardGate.gd
+++ UTS-QuantumComputingGame/LogicGates/HademardGate.gd
@@ -36,7 +36,7 @@ func destroy_after_movement():
 	logic_gate.destroy = true
 	set_movable(false)
 	remove_from_group("LogicGate")
-	logic_gate.destination = get_tree().get_nodes_in_group("bitButton")[0].position
+	logic_gate.destination = get_tree().get_nodes_in_group("HademardButton")[0].position
 
 func set_movable(status):
 	logic_gate.is_movable = status
